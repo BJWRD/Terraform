149691c25c198a72391f523bcec6b45f0284a61a
BJW <83971386+BJWRD@users.noreply.github.com>
2024-02-07 10:43:22 +0000

    Update variables.tf

--- rancher-terraform-eks-cluster/variables.tf
+++ rancher-terraform-eks-cluster/variables.tf
@@ -39,7 +39,7 @@ variable "nginx_additional_access_cidrs" {
 }
 
 variable "node_group" {
-  description = "Name of Arcus Instance"
+  description = "Name of Instance"
   type        = string
   default     = "Primary"
 }
